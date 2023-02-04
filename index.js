9b4f835b9827263874ec9345d4baf623e6c500a7
DE BARROS Geoffrey <debarrosg01@gmail.com>
2023-02-04 15:35:33 +0100

    Rest a gérer le custom % et un peu de css

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-let h1 = document.querySelector("h1");
-let body = document.querySelector("body");
-
+const h1 = document.querySelector("h1");
+const body = document.querySelector("body");
+let bill = document.getElementById("bill");
+const buttons = document.querySelectorAll("button");
+let people = document.getElementById("people");
+let totalBillPerPerson = document.getElementById("totalBillPerPerson");
+let TipPerPersonAmount = document.getElementById("TipPerPersonAmount");
+const reset = document.getElementById("reset");
 
 // Animation header
 window.addEventListener("load", () => {
@@ -9,3 +14,32 @@ window.addEventListener("load", () => {
     body.style.background = "hsl(185, 41%, 84%)";
   }, 500);
 });
+
+const billCalcul = () => {
+  bill.addEventListener("change", (event) => {
+    billValue = parseFloat(event.target.value);
+  });
+
+  buttons.forEach((button) => {
+    button.addEventListener("focus", (event) => {
+      let percentage = parseFloat(event.target.textContent) / 100;
+      totalBill = billValue * percentage;
+    });
+  });
+
+  people.addEventListener("change", (event) => {
+    peopleNb = parseFloat(event.target.value);
+    TipPerPersonAmount.innerHTML =
+      Math.round((totalBill / peopleNb) * 100) / 100;
+    totalBillPerPerson.innerHTML = billValue / peopleNb + totalBill / peopleNb;
+  });
+
+  reset.addEventListener("click", () => {
+    document.getElementById("bill").value = null;
+    document.getElementById("people").value = null;
+    TipPerPersonAmount.innerHTML = "00";
+    totalBillPerPerson.innerHTML = "00";
+  });
+};
+
+billCalcul();
